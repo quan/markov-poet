c02b55870c2fa06be4a0fdf4889de48fe0ebccb6
quan <tranmqt@gmail.com>
2017-12-07 02:01:46 -0800

    added randomness to the generator

--- src/Markov.py
+++ src/Markov.py
@@ -20,11 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from functools import reduce
 from enum import Enum
+from functools import reduce
 import random
-
-from haiku_loader import HaikuLoader
 
 EPSILON = 0.000001
 
@@ -43,10 +41,10 @@ class UntrainedModelError(Exception):
 
 class Token(Enum):
     """
-    Used to model special tokens in text: the START of a line and the END of a line.
+    Used to model special tokens in text: the start of a line and a newline.
     """
     START = '*'
-    END = '\n'
+    NEWLINE = '\n'
 
 
 class Markov:
@@ -58,14 +56,13 @@ class Markov:
         # The number of previous states the Markov Chain will consider.
         # Values above 2 are not recommended.
         self.order = order
-        # A set of all of the words in the language.
-        self.language = set()
+        
         # A mapping of words to a list of words that follow.
         self.graph = {}
 
     def add_file(self, filename):
         """
-        Train the model with a set of data read from a file.
+        Train the model with a corpus read from a file.
         """
         with open(filename, 'r') as file:
             text = file.read()
@@ -75,21 +72,24 @@ class Markov:
         """
         Add the words of a line to the chain.
         """
-        words = [Token.START] + line.split() + [Token.END]
+        words_in_line = list(map(lambda x: x.lower(), line.split()))
+        tokens = [Token.START] + words_in_line + [Token.NEWLINE]
 
-        # Define each word in the language and save word pairings in the graph.
-        # Don't iterate over the closing new line token.
-        for i in range(len(words) - 1):
-            word = words[i]
-            next_word = words[i + 1]
+        # Save word pairings in the graph to build the chain.
+        # Stop before the closing new line token to prevent IndexOutOfBounds.
+        for i in range(len(tokens) - 1):
+            word = tokens[i]
+            next_word = tokens[i + 1]
 
-            if word not in self.language:
-                self.language.add(word)
+            # Add each word to the language.
+            if word not in self.graph:
                 self.graph[word] = []
 
-            self.graph[word].append(next_word)
+            # Prevent empty lines from being added to the graph.
+            if word is not Token.START or next_word is not Token.NEWLINE:
+                self.graph[word].append(next_word)
 
-    def add_poem_as_list(self, poem):
+    def add_lines(self, poem):
         """
         Add the words in a poem to this Markov model's data.
         Expects a list of strings, where each string is a line in a poem.
@@ -103,66 +103,108 @@ class Markov:
         Expects a poem as a multi-line string.
         """
         poem_lines = poem.split('\n')
-        self.add_poem_as_list(poem_lines)
+        self.add_lines(poem_lines)
 
-    def generate(self, lines=3, randomness=0):
+    def generator(self, randomness=0.0):
+        """Return a poem generator for the Markov model with some randomness."""
+        return self.Generator(self.graph, randomness)
+
+    class Generator:
         """
-        Generate a poem from the training data with the given number of lines.
-        Return the poem as a list of lines.
+        A class that generates poems based on a given chain.
         """
-        if randomness < -EPSILON or randomness - 1.0 > EPSILON:
-            raise ValueError("Randomness should be a value between 0.0 and 1.0, inclusive")
+        def __init__(self, graph, randomness):
+            if not -EPSILON < randomness < 1.0 + EPSILON:
+                raise ValueError("Randomness should be a value between 0.0 and 1.0, inclusive")
 
-        if len(self.graph.keys()) == 0:
-            raise UntrainedModelError
+            self.graph = graph
 
-        poem = []
+            # The amount of randomness introduced into word generation.
+            self.randomness = randomness
 
-        for _ in range(lines):
-            poem.append(self.generate_line())
+            # A representative sample of all of the possible states in the chain.
+            # self.random_sample = reduce(lambda x, y: x + y, graph.values())
+            self.random_sample = []
+            for values in graph.values():
+                self.random_sample.extend(values)
 
-        return poem
+        def generate(self, lines=3):
+            """
+            Generate a poem from the training data with the given number of lines.
+            Return the poem as a list of lines.
+            """
+            if len(self.graph.keys()) == 0:
+                raise UntrainedModelError
 
-    def generate_formatted(self, lines=3, randomness=0):
-        """
-        Generate a poem based on the training data with the given number of lines.
-        Return the poem as a string.
-        """
-        generated_lines = self.generate(lines, randomness)
-        formatted_poem = '\n'.join(generated_lines)
+            poem = []
 
-        return formatted_poem
+            for _ in range(lines):
+                line = self.generate_line()
+                poem.append(line)
 
-    def generate_line(self, randomness=0):
-        """Generate a single line in a poem."""
-        words = []
+            return poem
 
-        # Select the first word by beginning with the start token.
-        state = Token.START
-        next_state = self._next_state(state)
+        def generate_formatted(self, lines=3):
+            """
+            Generate a poem based on the training data with the given number of lines.
+            Return the poem as a string.
+            """
+            generated_lines = self.generate(lines)
+            formatted_poem = '\n'.join(generated_lines)
 
-        # Follow the chain until a newline is reached.
-        while next_state is not Token.END:
-            state = next_state
-            words.append(state)
-            next_state = self._next_state(state)
+            return formatted_poem
 
-        line = ' '.join(words)
+        def generate_line(self):
+            """Generate a single line in a poem."""
+            words = []
 
-        return line
+            # Select the first word by beginning with the start token.
+            state = self._next_word(Token.START)
 
-    def _next_state(self, state):
-        """Randomly select the next state for a given state."""
-        possible_states = tuple(self.graph[state])
-        next_state = random.choice(possible_states)
+            # The nested outer loop and state reset if the words list is empty
+            # after the inner loop ensures that at least one word is generated.
+            while not words:
+                while state is not Token.NEWLINE:
+                    words.append(state)
+                    state = self._next_word(state)
 
-        return next_state
+                if not words:
+                    state = self._next_word(Token.START)
 
-    def debug_language_string(self):
-        """Create and return a string containing the language data."""
-        return sorted(list(self.language))
+            line = ' '.join(words)
 
-    def debug_graph_string(self):
+            return line
+
+        def _next_word(self, state):
+            """Randomly select the next word for a given state or simply a random word."""
+            if random.random() < self.randomness:
+                possibilities = tuple(self.random_sample)
+            else:
+                possibilities = tuple(self.graph[state])
+
+            next_word = random.choice(possibilities)
+
+            return self._next_word(state) if next_word is Token.START else next_word
+
+        def test_next_word(self):
+            """
+            Tests the asymptotic probabability of randomly selecting a new line.
+            """
+            sample_size = 10000
+            number_of_newlines = 0
+            state = Token.START
+
+            for i in range(sample_size):
+                state = self._next_word(state)
+                if state is Token.NEWLINE:
+                    number_of_newlines = number_of_newlines + 1
+                    i = i + 1
+                    state = Token.START
+                    continue
+
+            print("out of {} samples, {} were of newlines".format(sample_size, number_of_newlines))
+
+    def debug_string(self):
         """Create and return a string containing the graph data."""
         debug_string = ''
 
---
+++ src/demo.py
@@ -0,0 +1,51 @@
+from Markov import Markov
+from haiku_loader import HaikuLoader
+
+import random
+import argparse
+
+
+def main(args):
+    markov = Markov()
+
+    if args.filename is None:
+        print('No filename provided')
+        return
+    else:
+        markov.add_file(args.filename)
+
+    generator = markov.generator(args.randomness)
+
+    for _ in range(args.number):
+        print(generator.generate_formatted())
+        print()
+
+    # print(markov.debug_graph_string)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Poem generator command line tools.')
+
+    # Not yet implemented.
+    #
+    # parser.add_argument('-v', '--verbose',
+    #                     action='store_const',
+    #                     const=True, default=False,
+    #                     help='keep you updated as stuff happens')
+    parser.add_argument('-f', type=str,
+                        dest='filename',
+                        help='specify a file to read from')
+    parser.add_argument('-r', type=float,
+                        dest='randomness', default=0.0,
+                        help='introduce some randomness (between 0.0 and 1.0)')
+    parser.add_argument('-n', type=int,
+                        dest='number', default=1,
+                        help='Print more than one haiku')
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    main(args)
