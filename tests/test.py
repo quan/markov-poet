1eb3fe8baeb99b9b2b1a373784bd00dbb3b00c0c
quan tran <tranmqt@gmail.com>
2018-07-30 20:32:21 -0700

    missed make_gen

--- tests/test.py
+++ tests/test.py
@@ -12,18 +12,18 @@ class TestMarkov(unittest.TestCase):
     def test_invalid_randomness(self):
         """Test that generator creation fails with invalid randomness values."""
         with self.assertRaises(ValueError):
-            self.markov.generator(-200)
+            self.markov.make_generator(-200)
         with self.assertRaises(ValueError):
-            self.markov.generator(-0.1)
+            self.markov.make_generator(-0.1)
         with self.assertRaises(ValueError):
-            self.markov.generator(40)
+            self.markov.make_generator(40)
         with self.assertRaises(ValueError):
-            self.markov.generator(1.1)
+            self.markov.make_generator(1.1)
 
     def test_untrained_model(self):
         """Test that generating with an untrained model raises an exception."""
         with self.assertRaises(UntrainedModelError):
-            self.markov.generator()
+            self.markov.make_generator()
 
 # class TestGeneration(unittest.TestCase):
     
